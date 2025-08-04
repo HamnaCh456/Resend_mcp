1a60e79a19a31aa5fcab7e246a7db37624e25228
Hamna_Hameed <164740569+HamnaCh456@users.noreply.github.com>
2025-08-05 01:58:06 +0500

    initial_commit

---
+++ server_mcp.py
@@ -0,0 +1,33 @@
+from mcp.server.fastmcp import FastMCP  
+import resend  
+import httpx
+from dotenv import load_dotenv
+
+PORT = os.environ.get("PORT", 10000)
+mcp = FastMCP("Resend Email Server",, host="0.0.0.0", port=PORT) 
+
+@mcp.tool()   
+def send_email(subject: str, to: str, from_email: str, content: str, api_key: str) -> str:  
+    """Sends an email using the Resend API."""  
+    try:  
+        if not api_key or api_key == "YOUR_RESEND_API_KEY":  
+            raise ValueError("Resend API key not provided or invalid.")  
+          
+        import resend  
+        resend.api_key =os.getenv("RESEND_API")  
+          
+        params = {  
+            "from": from_email,  
+            "to": [to],  
+            "subject": subject,  
+            "html": content,  
+        }  
+        email = resend.Emails.send(params)  
+        return f"Email sent successfully"  
+    except ImportError as e:  
+        return f"Import error: {e}. Make sure 'resend' package is installed."  
+    except Exception as e:  
+        return f"Email sending failed: {str(e)}"
+
+if __name__ == "__main__":
+    mcp.run(transport="http")
